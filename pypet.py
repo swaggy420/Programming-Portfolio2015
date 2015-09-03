4536eba9b37349d9bf349505da5eee6eb21fc554
Erik Hughes <erikhughes@Alveyworld-15.local>
2015-09-03 10:30:48 -0600

    changes

--- pypet.py
+++ pypet.py
@@ -1,16 +1,30 @@
 print 'Welcome to Pypet!'
 
-name = 'Fluffy'
+name = ' Jerry'
+age = 6
+weight = 1.5
+hungry = False
+photo = '<:3 )~~~~'
+
+
+print 'Hello' + name
+print photo
+mouse = {
+	'name': ' Jerry',
+	'age': 6,
+	'weight': 1.5,
+	'hungry': False,
+	'photo': '<:3 )~~~~',
+}
+
+name = ' Tom'
 age = 5
 weight = 9.5 
 hungry = False 
 photo = '(=^o.o^=)__'
 
-
-print 'Hello' + name
-print photo 
-cat {
-	'name': 'Fluffy',
+cat = {
+	'name': ' Tom',
 	'hungry': True,
 	'weight': 9.5,
 	'age': 5,
@@ -27,11 +41,18 @@ def feed(pet):
 
 feed(cat)
 def feed(pet):
-if pet['hugnry'] == True:
-	pet['hungry'] = False
-	pet['weight'] = pet['weight'] + 1
-else:
-	print 'The Pypet is not hungry'
+	if pet['hungry'] == True:
+		pet['hungry'] = False
+		pet['weight'] = pet['weight'] + 1
+	else:
+		print 'The Pypet is not hungry'
+
+pets = [cat, mouse]
+
+for pet in pets:
+	feed(pet)
+	print pet
+
 print cat
 feed(cat)
 print cat
